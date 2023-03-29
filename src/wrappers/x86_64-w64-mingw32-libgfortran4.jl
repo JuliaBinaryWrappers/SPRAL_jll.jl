c62d71886f24c7e9aa2c466f894e5f2ddd11991b
jlbuild <juliabuildbot@gmail.com>
2023-03-29 22:44:14 +0000

    SPRAL_jll build 2023.3.29+0

--- src/wrappers/x86_64-w64-mingw32-libgfortran4.jl
+++ src/wrappers/x86_64-w64-mingw32-libgfortran4.jl
@@ -3,13 +3,13 @@ export libspral, spral_ssids
 
 using METIS_jll
 using CompilerSupportLibraries_jll
-using OpenBLAS32_jll
+using libblastrampoline_jll
 using Hwloc_jll
 JLLWrappers.@generate_wrapper_header("SPRAL")
 JLLWrappers.@declare_library_product(libspral, "libspral.dll")
 JLLWrappers.@declare_executable_product(spral_ssids)
 function __init__()
-    JLLWrappers.@generate_init_header(METIS_jll, CompilerSupportLibraries_jll, OpenBLAS32_jll, Hwloc_jll)
+    JLLWrappers.@generate_init_header(METIS_jll, CompilerSupportLibraries_jll, libblastrampoline_jll, Hwloc_jll)
     JLLWrappers.@init_library_product(
         libspral,
         "bin\\libspral.dll",
